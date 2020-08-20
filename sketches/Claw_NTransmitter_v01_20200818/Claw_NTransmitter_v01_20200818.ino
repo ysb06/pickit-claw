17fbeb2fda2765adcc9f8a003a6749ec9fdb6b42
Seungbin Yim <ysb06@hotmail.com>
2020-08-20 18:00:09 +0900

    Add servo motor controller
    Signed-off-by: Seungbin Yim <ysb06@hotmail.com>

--- sketches/Claw_NTransmitter_v01_20200818/Claw_NTransmitter_v01_20200818.ino
+++ sketches/Claw_NTransmitter_v01_20200818/Claw_NTransmitter_v01_20200818.ino
@@ -22,17 +22,20 @@ void loop()
 {
   byte reading = digitalRead(BUTTON_PIN);  //버튼 눌렸는지 검사
   
-  byte packet[5] = {0, };
+  byte packet[6] = {0, };
   packet[0] = 0x02;     // STX
   packet[1] = 0x00;     // 제어 코드 예약, 현재는 의미 없음
   packet[2] = channel;  // 채널
   packet[3] = reading;  // 버튼 데이터
-  packet[4] = 0x03;     // ETX
+  packet[4] = 100;      // 체크섬
+  packet[5] = 0x03;     // ETX
   
   Serial.print("Sending: [chan: ");
   Serial.print(packet[2]);
   Serial.print(", data: ");
-  Serial.print(reading);
+  Serial.print(packet[3]);
+  Serial.print(", checksum: ");
+  Serial.print(packet[4]);
   Serial.print("]...");
   
   LoRa.beginPacket();
